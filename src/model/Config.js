865351586aa3ed9c20923302f04bffa233591d3e
Sriram Karthick G <sriram.karthick@grootan.com>
2021-05-17 11:38:29 +0530

    Initial Commit
    Initial commit for keycloak's oidc implementation with vuejs

---
+++ src/model/Config.js
@@ -0,0 +1,6 @@
+export const CookieName = "Keycloak_Auth";
+export const Config = {
+    "url": "https://{hostname}/auth",
+    "realm": "keycloak-tenant-id",
+    "clientId": "keycloak-client-id"
+}
